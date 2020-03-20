50db4c07eda31165832d921edde426b7448a6dc8
Ivanildo de Castro <ivanildo.decastro085@gmail.com>
2020-03-20 13:01:35 -0400

    feat: initial setup

---
+++ api-2/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/', function (req, res) {
+  res.send('Hello World! Test 2');
+});
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}`);
+});
